fdaeb9fb45affd86968cca7d8826dc6a4cc354e8
Jordan Yoshihara <jordanyoshihara@gmail.com>
2019-07-30 17:20:08 -0700

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 
 from setuptools import find_packages, setup
 
-long_description = open('README.md').read()
+readme = open('README.md').read()
+
+with open('docs/history.rst') as history_file:
+    history = history_file.read()
 
 requirements = [
-    "pycountry==17.5.14",
+    "requests>=2.11.1",
+    "beautifulsoup4>=4.6.3",
+    "ricecooker>=0.6.31",
+    "le_utils>=0.1.19",
+    "cssutils>=1.0.2",
+    "google-api-python-client>=1.7.9",
+    "google-auth-oauthlib>=0.4.0",
+    "oauthlib>=3.0.1",
+    "requests-oauthlib>=1.2.0",
+    "youtube_dl>=2019.7.2",
 ]
+
 
 setup(
     name="webmixer",
     packages = find_packages(),
     version="0.0.0",
     description="Library for scraping urls and downloading them as files",
-    long_description=long_description,
+    long_description=readme + '\n\n' + history,
     long_description_content_type="text/markdown",
     install_requires=requirements,
     license="MIT",
@@ -27,6 +40,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    author="Jordan Yoshihara",
-    author_email="jordan@learningequality.org",
+    author="Learning Equality",
+    author_email='dev@learningequality.org',
 )
